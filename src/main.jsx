32427d3c8dc4de47d0b8a4812f58b833200cb02b
Arima_Key <127151745+ArimaKey@users.noreply.github.com>
2023-05-18 17:09:06 -0500

    v 0.0.4

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { AppRouter } from './Routers/AppRouter'
 import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
-
+import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
